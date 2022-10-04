032da3189ea738ce9398ca09c6b01e28b017976f
Kav Singh <flakisms@gmail.com>
2022-10-04 23:59:27 +0200

    update links

--- scripts/lib/constants.ts
+++
@@ -1,6 +0,0 @@
-import { resolve } from "https://deno.land/std/path/mod.ts";
-import { __ } from "https://deno.land/x/dirname/mod.ts";
-
-const { __filename, __dirname } = __(import.meta);
-
-export const PROJECT_ROOT = resolve(__dirname, "../../");
--- scripts/updatePage.ts
+++
@@ -1,6 +0,0 @@
-import projects from "../projects.ts";
-import updateIndex from "./lib/updateIndex.ts";
-
-updateIndex(projects)
-  .then(() => console.log("index.html updated"))
-  .catch((error) => console.error(error));
